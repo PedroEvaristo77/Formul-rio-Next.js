a04f34585991accbb3cece438397c2f01c4c616c
Pedro Evaristo <galvaoevaristo@gmail.com>
2022-05-04 13:23:23 -0300

    Cadastro 2.0

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -4,7 +4,7 @@ interface BotaoProps {
 }
 export default function Botao(props:BotaoProps){
     return(
-        <button className="bg-green-100 p-1 rounded-md mb-3">
+        <button onClick={props.onClick}className="bg-green-100 p-1 rounded-md mb-3">
             {props.children}
         </button>
     )
